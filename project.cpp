21fda525066debbf21d7cb105e2f25e3f822a765
Sumaya Rahman <127914406+RahmanSumaya@users.noreply.github.com>
2025-12-15 21:07:07 +0600

    w

---
+++ project.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<fstream>
+#include <sstream>
+using namespace std;
+
+int main{
+    cout<<"\t\t\t**************************************"<<endl;
+        cout<<"\t\t\t=                                    ="<<endl;
+        cout<<"\t\t\t=              Welcome To            ="<<endl;
+        cout<<"\t\t\t=        To-Do List Application      ="<<endl;
+        cout<<"\t\t\t=                                    ="<<endl;
+        cout<<"\t\t\t=                                    ="<<endl;
+        cout<<"\t\t\t**************************************"<<endl;
+
+        string xx;
+        cout<<endl<<endl<<"\t\t\tPress Any Key To Continue : ";
+        cin>>xx;
+        system("cls");
+        cout<<"kaam kortese omagawd"<<endl;
+    }
+
+

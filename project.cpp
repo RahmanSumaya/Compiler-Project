18448a55ba150b58aae64f67bbe5ae33fcd5a760
Sumaya Rahman <127914406+RahmanSumaya@users.noreply.github.com>
2025-12-15 21:14:58 +0600

    h

--- project.cpp
+++ project.cpp
@@ -6,20 +6,20 @@
 #include <sstream>
 using namespace std;
 
-int main{
+int main(){
     cout<<"\t\t\t**************************************"<<endl;
-        cout<<"\t\t\t=                                    ="<<endl;
-        cout<<"\t\t\t=              Welcome To            ="<<endl;
-        cout<<"\t\t\t=        To-Do List Application      ="<<endl;
-        cout<<"\t\t\t=                                    ="<<endl;
-        cout<<"\t\t\t=                                    ="<<endl;
-        cout<<"\t\t\t**************************************"<<endl;
+      cout<<"\t\t\t=                                    ="<<endl;
+    cout<<"\t\t\t=              Welcome To            ="<<endl;
+     cout<<"\t\t\t=        To-Do List Application      ="<<endl;
+     cout<<"\t\t\t=                                    ="<<endl;
+      cout<<"\t\t\t=                                    ="<<endl;
+      cout<<"\t\t\t**************************************"<<endl;
 
-        string xx;
-        cout<<endl<<endl<<"\t\t\tPress Any Key To Continue : ";
-        cin>>xx;
-        system("cls");
-        cout<<"kaam kortese omagawd"<<endl;
-    }
+     string xx;
+     cout<<endl<<endl<<"\t\t\tPress Any Key To Continue : ";
+     cin>>xx;
+     system("cls");
+     cout<<"kaam kortese omagawd"<<endl;
+  }
 
 
